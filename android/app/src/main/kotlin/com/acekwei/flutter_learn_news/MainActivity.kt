a18212c68e0e9232880cd50575320bf59f605cfd
AceKwei <gj3017@163.com>
2021-12-19 17:19:49 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/acekwei/flutter_learn_news/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.acekwei.flutter_learn_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn_news/pages/welcome/welcome.dart';
+import 'package:flutter_learn_news/routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ducafecat.tech',
+      home: WelcomePage(),
+      routes: staticRoutes,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
